ba75896920c9943549387cf776a3bde3148ff121
sadiyas-cloud-kitchen <cloudsadiya@gmail.com>
2026-01-07 13:53:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// Hero slider
-const slides = document.querySelectorAll('.slide');
-let index = 0;
+/* HERO SLIDER */
+let heroSlides = document.querySelectorAll('.slide');
+let heroIndex = 0;
 
 setInterval(() => {
-  slides[index].classList.remove('active');
-  index = (index + 1) % slides.length;
-  slides[index].classList.add('active');
-}, 4500);
+  heroSlides[heroIndex].classList.remove('active');
+  heroIndex = (heroIndex + 1) % heroSlides.length;
+  heroSlides[heroIndex].classList.add('active');
+}, 4000);
 
-// Dessert slider
-const desserts = document.querySelectorAll('.dessert');
+/* DESSERT SLIDER */
+let desserts = document.querySelectorAll('.dessert');
 let dessertIndex = 0;
 
 setInterval(() => {
   desserts[dessertIndex].classList.remove('active');
   dessertIndex = (dessertIndex + 1) % desserts.length;
   desserts[dessertIndex].classList.add('active');
-}, 3500);
+}, 3000);
