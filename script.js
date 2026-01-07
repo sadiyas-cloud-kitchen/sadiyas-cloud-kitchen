41fa27afda39eeb7856a2f26e6db564163f98e5f
sadiyas-cloud-kitchen <cloudsadiya@gmail.com>
2026-01-07 16:31:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.body.classList.add("loaded");
+const tabs = document.querySelectorAll(".tab");
+const groups = document.querySelectorAll(".menu-group");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(t => t.classList.remove("active"));
+    groups.forEach(g => g.classList.remove("active"));
+
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
+  });
+});
