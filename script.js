dd9a09697f3e9db45cca0c1b8bb9e922a0917ae3
sadiyas-cloud-kitchen <cloudsadiya@gmail.com>
2026-01-07 15:45:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-/* HERO SLIDER */
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
-
-setInterval(() => {
-  slides[currentSlide].classList.remove("active");
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add("active");
-}, 4000);
-
-/* DESSERT SLIDER */
-const desserts = document.querySelectorAll(".dessert");
-let dessertIndex = 0;
-
-setInterval(() => {
-  desserts[dessertIndex].classList.remove("active");
-  dessertIndex = (dessertIndex + 1) % desserts.length;
-  desserts[dessertIndex].classList.add("active");
-}, 3000);
+// Reserved for future animations
+console.log("Sadiya's Cloud Kitchen loaded");
