c4aec5dda69a4125801231df5f6c4df5af409b0a
sadiyas-cloud-kitchen <cloudsadiya@gmail.com>
2026-01-07 17:05:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const tabs = document.querySelectorAll(".tab");
-const groups = document.querySelectorAll(".menu-group");
+let cart=[]
+let count=document.getElementById("count")
 
-tabs.forEach(tab => {
-  tab.addEventListener("click", () => {
-    tabs.forEach(t => t.classList.remove("active"));
-    groups.forEach(g => g.classList.remove("active"));
+function addToCart(name,price){
+  cart.push({name,price})
+  count.innerText=cart.length
+}
 
-    tab.classList.add("active");
-    document.getElementById(tab.dataset.tab).classList.add("active");
-  });
-});
+function sendWhatsApp(){
+  if(cart.length===0){
+    alert("Cart is empty")
+    return
+  }
+
+  let msg="Hello, I want to order:%0A"
+  let total=0
+
+  cart.forEach(i=>{
+    msg+=`• ${i.name} - ₹${i.price}%0A`
+    total+=i.price
+  })
+
+  msg+=`%0ATotal: ₹${total}`
+  window.open(`https://wa.me/917549126937?text=${msg}`)
+}
+
+/* TAB SWITCH */
+document.querySelectorAll(".tab").forEach(tab=>{
+  tab.onclick=()=>{
+    document.querySelectorAll(".tab").forEach(t=>t.classList.remove("active"))
+    document.querySelectorAll(".menu").forEach(m=>m.classList.remove("active"))
+    tab.classList.add("active")
+    document.getElementById(tab.dataset.tab).classList.add("active")
+  }
+})
