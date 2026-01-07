a943d824eb20203d55c2bac92b89c7ff21324af6
sadiyas-cloud-kitchen <cloudsadiya@gmail.com>
2026-01-07 12:08:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const slides = document.querySelectorAll('.slide');
+let slides = document.querySelectorAll('.slide');
 let index = 0;
 
-function showSlide() {
-  slides.forEach(s => s.classList.remove('active'));
-  slides[index].classList.add('active');
+setInterval(() => {
+  slides[index].classList.remove('active');
   index = (index + 1) % slides.length;
-}
-
-setInterval(showSlide, 4500);
-
-// PAGE LOADER
-window.addEventListener('load', () => {
-  document.getElementById('loader').style.display = 'none';
-});
+  slides[index].classList.add('active');
+}, 4500);
